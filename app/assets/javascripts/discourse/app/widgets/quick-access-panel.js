dfcc72550ac9ca6483db1a7eb55a8baa3f9bd3c1
romanrizzi <rizziromanalejandro@gmail.com>
2021-01-19 15:36:03 -0300

    A11Y: Improve the quick access menu accessibility.
    - The icon for the “view all” controls in the panels have no accessible alternative.
    - Because the “Log Out” and "Do Not Disturb" elements in the preferences tab are an <a> element without an href attribute, it is not keyboard focusable and therefore not keyboard focusable. Use a button element instead.

--- app/assets/javascripts/discourse/app/widgets/quick-access-panel.js
+++ app/assets/javascripts/discourse/app/widgets/quick-access-panel.js
@@ -108,6 +108,7 @@ export default createWidget("quick-access-panel", {
           title: "view_all",
           icon: "chevron-down",
           className: "btn btn-default btn-icon no-text show-all",
+          alt: I18n.t("view_all"),
           href: this.showAllHref(),
         })
       );
